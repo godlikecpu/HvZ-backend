40f5f93913333d58c1cace43d9cf39aae2ef5158
GodlikeCPU <dan.tomicic@gmail.com>
2021-03-17 09:17:21 +0100

    Initial commit

---
+++ src/test/java/com/experis/hvzbackend/HvzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.hvzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
